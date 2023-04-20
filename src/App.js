4d92e20d97eac123335bb72c50ee782132bb2566
cbrlpdr <cbrl.pdr@gmail.com>
2023-04-20 19:18:46 +0000

    Removing Footer from App since it's not complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React, { useCallback, useState } from 'react';
 import { Button } from './Components/Button';
 import { Display } from './Components/Display';
-import { Footer } from './Components/Footer';
 import { Description } from './Components/Description';
 
 function App() {
@@ -18,7 +17,6 @@ function App() {
                 <Display counter={counter} />
                 <Description version={'0.1'} />
                 <Button fn={handleCount} />
-                <Footer />
             </div>
         </>
     );
